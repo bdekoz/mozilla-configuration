f25887fba37eeb4bb8908a2ca353c50f644ec9d4
Benjamin De Kosnik <bdekoz@mozilla.com>
2019-10-08 11:26:08 -0700

    2019-10-08  Benjamin De Kosnik  <bdekoz@mozilla.com>
    	* scripts/*: Consitent canonical directory.

--- scripts/run-ff-browse.sh
+++ scripts/run-ff-browse.sh
@@ -1,3 +1,3 @@
-MDIR=/home/bkoz/src/mozilla-configuration
+MDIR=/home/bkoz/src/mozilla-configuration.git
 FF=$bin/H-firefox-nightly-x86-64/bin/firefox
 $FF --no-remote --profile $MDIR/profile-browse
--- scripts/run-ff-profile-manager.sh
+++ scripts/run-ff-profile-manager.sh
@@ -1,3 +1,3 @@
-MDIR=/home/bkoz/src/mozilla-config
+MDIR=/home/bkoz/src/mozilla-configuration.git
 FF=$bin/H-firefox-nightly/firefox
 $FF --no-remote --ProfileManager
