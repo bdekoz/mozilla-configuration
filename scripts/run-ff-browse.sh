fce3ae81bf7daeafd1eb34762f899a17429126cc
Benjamin De Kosnik <bdekoz@mozilla.com>
2020-01-07 13:44:28 -0800

    2020-01-07  Benjamin De Kosnik  <bdekoz@mozilla.com>
    	* scripts/run-ff-browse.sh: Add URL argument.

--- scripts/run-ff-browse.sh
+++ scripts/run-ff-browse.sh
@@ -1,3 +1,4 @@
+URL=$1
 MDIR=/home/bkoz/src/mozilla-configuration.git
 FF=$bin/H-firefox-nightly/firefox
-$FF --no-remote --profile $MDIR/profile-browse
+$FF --no-remote --profile $MDIR/profile-browse $URL
