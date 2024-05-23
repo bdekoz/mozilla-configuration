e0d6ec3f1acf1c488ccb94272c018b3ae5d39a62
Benjamin De Kosnik <bkoz@gnu.org>
2024-05-23 11:09:41 -0700

    moz-environment.bash: update node to specific version

--- moz-environment.bash
+++ moz-environment.bash
@@ -1,14 +1,13 @@
-
 # Setup mozilla config files
 export MOZHOME=/home/bkoz/src/mozilla-configuration.git
 export MOZCONFIG=$MOZHOME/mozconfig
 
 # Setup mozilla node environment (22.0)
 export XDG_CONFIG_HOME=/home/bkoz/src/nvm
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm use 22
 
 # Setup mozilla python environment (3.10)
 . $MOZHOME/moz-python/bin/activate.bash
-
 echo `python --version`
